2389dd6379db08a81a699b3316a3196680dc0619
Mundhra <smundhra@intul168720a73.corp.intuit.net>
2019-02-20 11:35:18 +0530

    added and solved issue

---
+++ src/digitSum.js
@@ -0,0 +1,6 @@
+export const digitSum = (number) => {
+  while (number) {
+      sum += value % 10;
+      value = Math.floor(value / 10);
+  }
+};
---
+++ src/isPalindromeString.js
@@ -0,0 +1,3 @@
+export const isPalindromeString = () => {
+  // TO IMPLEMENT IN ANOTHER PR
+};
